04b8834cba8dd36c55d1f8f4e6946613b04832a4
Saubhagya Maheshwari <saubhagyamaheshwari@gmail.com>
2012-06-14 23:50:29 +0530

    Added serveFile utility.

--- utility.js
+++ utility.js
@@ -1,3 +1,6 @@
+var path = require('path'),
+    fs   = require('fs');
+
 var isValidType = function(validTypes, fileName) {
     var i = fileName.lastIndexOf('.'),
         ext = (i < 0) ? '' : fileName.substr(i);
@@ -5,12 +8,34 @@ var isValidType = function(validTypes, fileName) {
     return validTypes.indexOf(ext) == -1 ? false : true;
 };
 
-var getDirFiles = function(dir) {
+var getDirFiles = function(dir, callback) {
     fs.readdir(dir, function(err, list) {
         if (err) throw err;
-        // Do the needful
+        callback(list);
+    });
+};
+
+var serveFile = function(res, uri) {
+    path.exists(uri, function(exists) {
+        if (exists) {
+            fs.readFile(uri, function(error, content) {
+                if (error) {
+                    res.writeHead('500');
+                    res.end();
+                } else {
+                    res.writeHead('200');
+                    res.write(content);
+                    res.end();
+                }
+            });
+        } else {
+            res.writeHead('404');
+            res.end();
+        }
     });
 };
 
 exports.isValidType = isValidType;
 exports.getDirFiles = getDirFiles;
+exports.serveFile   = serveFile;
+
